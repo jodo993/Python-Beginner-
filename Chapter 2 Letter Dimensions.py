817367589a234b6ff59c8e5988b31a513bf58224
jodo993 <josephdo993@yahoo.com>
2016-09-05 01:45:30 -0700

    Chapter 2 Assignment

---
+++ Chapter 2 Letter Dimensions.py
@@ -0,0 +1,13 @@
+# This program display the dimensions of a letter size paper in millimeters
+# 8.5 x 11 inch
+MILLIMETERS_PER_INCH = 24.3
+
+
+# Width and height of a regular paper in inches
+WIDTH_OF_PAPER = 8.5
+HEIGHT_OF_PAPER = 11
+
+widthInMillimeters = MILLIMETERS_PER_INCH * WIDTH_OF_PAPER
+heightInMillimeters = MILLIMETERS_PER_INCH * HEIGHT_OF_PAPER
+
+print(str(widthInMillimeters) + "x" + str(heightInMillimeters))
