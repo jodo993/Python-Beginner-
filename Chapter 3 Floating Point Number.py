6e798192fb6f8261983d0381e40c0826c5600834
jodo993 <josephdo993@yahoo.com>
2016-09-14 00:26:00 -0700

    Chapter 3 Floating Point Number (Finished)

---
+++ Chapter 3 Floating Point Number.py
@@ -0,0 +1,22 @@
+# This program decide if a number is zero, positive, or negative.
+# It also determine if the number is small or large
+
+# Prompt user input
+number = input("Please enter a number: ")
+number = float(number)
+
+# Determines if number is 0, +, or -
+if number == 0:
+    print("Zero")
+elif number > 0:
+    print("Positive")
+else:
+    print("Negative")
+
+# Determines if the value is large or small
+absoluteValue = abs(number)
+absoluteValue = float(absoluteValue)
+if absoluteValue < 1:
+    print("Small")
+elif absoluteValue > 1000000:
+    print("Large")
