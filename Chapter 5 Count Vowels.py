f9354f926d4fdf0e27e5d7f75f98fd3f77cfe1f5
jodo993 <josephdo993@yahoo.com>
2016-10-03 23:50:33 -0700

    Chapter 5 Final
    final version with comments

---
+++ Chapter 5 Count Vowels.py
@@ -0,0 +1,11 @@
+def main():
+    print("This program will tell you the vowels in a word.")
+    word = input("Please enter a word: ")
+
+    countVowels(word)
+
+def countVowels(string)
+    for char in string:
+        if char in "aeiou":
+            string = char[]
+    print(char[])
