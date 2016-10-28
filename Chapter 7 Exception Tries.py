7f3f339c1a36ba95d8df4e44b0a1bd71ffbc28b9
jodo993 <josephdo993@yahoo.com>
2016-10-27 22:26:12 -0700

    Chapter 7 Final

--- Chapter 7 Exception Tries.py
+++ Chapter 7 Exception Tries.py
@@ -1,17 +1,31 @@
+# This program ask user to input float numbers
+# After two invalids, display total sum of valid inputs
+
+ok = True           # Start out as true
+total = 0.0         # Initialized to 0
+tries = 2           # Two tries
+
+# Ask first input
 print("Enter a set of floating point numbers.")
-numbers = float(input(""))
+print("After two chances, will add up total sum.")
+numbers = input("")
 
-ok = True
-total = 0.0
-tries = 2
-
-while tries > 0:
+# While tries still good, do
+while ok:
     try:
-        if ok:
-            total = total + numbers
-        else:
-            tries = tries - 1
-            numbers = float(input(""))
+        numbers = float(numbers)
+        total = total + numbers
+        numbers = input("")
+    except Exception as string:
+        print("Invalid")
+        tries = tries - 1
+        if (tries == 0):        # No more tries set of false
+            ok = False
+        if tries > 0:
+            numbers = input("")
+        
+# Display sum
+print("Total is: ", total)
             
                 
         
