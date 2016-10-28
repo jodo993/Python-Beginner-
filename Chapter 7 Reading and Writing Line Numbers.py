aad963f6b80776261599f794a0ece953aefc0108
jodo993 <josephdo993@yahoo.com>
2016-10-27 22:26:24 -0700

    Chapter 7 Final

--- Chapter 7 Reading and Writing Line Numbers.py
+++ Chapter 7 Reading and Writing Line Numbers.py
@@ -1,13 +1,18 @@
-
-inputFile = open("input.txt", "r")
+# This program reads a file and adds line number to each line
+# Open file to read and write
+inputFile = open("maryLamb.txt", "r")
 outputFile = open("output.txt", "w")
-line = inputFile.readline()
+
+# i keeps track of lines
 i = 1
-    
+line = inputFile.readline()
+
+# If not blank, add lines number to each line
 while line != "":
+    print("/*", i, "*/", line, file=outputFile)
     line = inputFile.readline()
-    outputFile.write("/*", i, "*/", line)
     i = i + 1
 
+# Close files
 inputFile.close()
 outputFile.close()
