c9a5fbbd46526bb0d0851b389f16562bb950b1fb
jodo993 <josephdo993@yahoo.com>
2016-11-09 22:18:58 -0800

    Chapter 9 Draft
    tally maximum

--- tallyMaximum.py
+++ tallyMaximum.py
@@ -1,16 +1,23 @@
 class Tally :
 
+    def __init__(self) :
+        self._count = 0
+        
+    def setlimit(self, maximum) :
+        self._maximum = maximum
+        
     def reset(self) :
         self._value = 0
 
     def click(self) :
         self._value = self._value + 1
+        self._count = self._count + 1
 
     def getValue(self) :
-        return self._value
+        if self._count <= self._maximum :
+            return self._value
+        else :
+            return print("Limit exceeded")
 
-    def setlimit(self, maximum) :
-        if self._value > maximum :
-            print("Limit exceeded")
             
         
