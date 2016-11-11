a1670de5904cddbaffbb5738155516415d6107c1
jodo993 <josephdo993@yahoo.com>
2016-11-10 21:54:05 -0800

    Chapter 9 Final

--- Chapter 9 Tally Undo Main.py
+++ Chapter 9 Tally Undo Main.py
@@ -1,19 +1,21 @@
+# This program will allow user to click then undo the click
 from tallyUndoClass import Tally
 
+# Create an object of the class Tally
 tally = Tally()
 
-tally.reset()
-tally.click()
+tally.reset()       # Reset the tally to 0
+tally.click()       # Add one to click per call
 tally.click()
 
-result = tally.getValue()
+result = tally.getValue()   # Find the current value of click
 print(result)
 
 tally.click()
 result = tally.getValue()
 print(result)
 
-tally.undo()
+tally.undo()        # Subtract one to click per call
 tally.undo()
 result = tally.getValue()
 print(result)
--- tallyUndoClass.py
+++ tallyUndoClass.py
@@ -1,18 +1,19 @@
 class Tally :
 
     def __init__(self) :
-        self._count = 0
+        self._count = 0     # Keeps track of how many clicks
         
     def reset(self) :
-        self._value = 0
+        self._value = 0     # Reset click to 0
 
     def click(self) :
-        self._value += 1
-        self._count = self._count + 1
+        self._value += 1    # Add one to click
+        self._count = self._count + 1   # Add one to count
         
-    def getValue(self) :
+    def getValue(self) :    # Return number of clicks
         return self._value
 
+    # If above 0, subtract one, else 
     def undo(self) :
         if self._count > 0 :
             self._value -= 1
