031cb16bcbb73261bc2f2e4685c6704333e09302
jodo993 <josephdo993@yahoo.com>
2016-11-10 22:33:44 -0800

    Chapter 9 Final

--- Chapter 9 Address Class.py
+++ Chapter 9 Address Class.py
@@ -1,12 +1,16 @@
+# This program takes the address information and display then see which one comes first
 from address import Address
 
+# Create address objects
 addressOne = Address(2500, "University Drive", "Some City", "Some State", "12345")
 addressTwo = Address(1200, "College Blvd", "Other City", "Other State", "99102", "12")
 
+# Get the address and display it
 addressOneResult = addressOne.printt()
 addressTwoResult = addressTwo.printt()
 print(addressOneResult)
 print(addressTwoResult)
 
+# Finds out with address comes first based on postal code
 comesBeforeResult = addressOne.comesBefore(addressTwo)
 print(comesBeforeResult)
--- address.py
+++ address.py
@@ -1,5 +1,6 @@
 class Address :
-        
+
+    # Initialize house, street, city, state, zip, and apt #
     def __init__(self, houseNumber, street, city, state, postalCode, apartmentNumber = "") :
         self._houseNumber = houseNumber
         self._street = street
@@ -8,10 +9,12 @@ class Address :
         self._postalCode = postalCode
         self._apartmentNumber = apartmentNumber
 
+    # Print the address on two lines
     def printt(self) :
-        print(self._street)
-        print(self._city, self._state, self._postalCode)
+        print (self._street) 
+        print (self._city, self._state, self._postalCode)
 
+    # Compares which one comes first
     def comesBefore(self, other) :
         if self._postalCode < other._postalCode :
-            return print("Address 1 comes first.")
+            return ("Address 1 comes first.")
