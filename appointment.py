5f45a215045e23060644e50f1125941e874bc654
jodo993 <josephdo993@yahoo.com>
2016-11-24 21:06:24 -0800

    Chapter 10 Draft

--- appointment.py
+++ appointment.py
@@ -1,9 +1,42 @@
 class Appointment:
 
-    
+    def __init__(self, day, month, year, description):
+        self._description = description
+        self._date = date(day,month,year)
+
+    def occursOn(self, day, month, year):
+        if self._date = date(day,month,year)
+            return True
+        else:
+            return False    
 
 class Onetime(Appointment):
+    
+    def __init__(self, day, month, year, description):
+        super(OneTime, self).__init__(day, month, year, description)
+
+    def occursOn(date):
+        return true;
 
 class Daily(Appointment):
 
+    def __init__(self, day, month, year, description):
+        super(Daily, self).__init__(day, month, year, description)
+
+    def occursOn(date):
+        return true;
+
 class Monthly(Appointment):
+
+    def __init__(self,day,month,year,description):
+       super(Monthly,self).__init__(day,month,year,description)
+
+    def occursOn(date):
+        return true;
+
+def occursOn(year, month, day):
+
+     if self._date == day:
+           return True
+       else:
+           return False
