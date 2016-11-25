1d1b12c539678c4f72d89a751180a66837b7a15b
jodo993 <josephdo993@yahoo.com>
2016-11-24 17:54:16 -0800

    Chapter 10 Check Answer Final

--- Chapter 10  Check Answer.py
+++ Chapter 10  Check Answer.py
@@ -1,5 +1,6 @@
 from questions import Question
 
+# Create ques
 question = Question()
 
 # Create question and answer
@@ -9,6 +10,6 @@ question.setAnswer("Guido van Rossum")
 # Display question and obtain user's response
 question.display()
 response = input("Your answer: ")
-question.modifiedAnswer(response)
+#question.modifiedAnswer(response)
 print(question.checkAnswer(response))
 
