2b2fdf1fd51b7cb750a7555549bd3ad4278f5cf3
jodo993 <josephdo993@yahoo.com>
2016-12-01 21:37:36 -0800

    Chapter 11 Final
    Reverse string iteration

--- Chapter 11 Reverse String Iteration.py
+++ Chapter 11 Reverse String Iteration.py
@@ -1,12 +1,18 @@
+# This program reverses a string by using iteration
+
 def reverse(text):
+    # Start as empty string
     reverse = ''
+    # Loop to add each char at a time backwards
     for i in range(len(text)):
         reverse = reverse + text[len(text)-i-1]
     return reverse
 
+# Send string to function and display
 def main():
     r = reverse("Hello!")
     print(r)
     print("Expected: !olleH")
 
+# Start program
 main()
