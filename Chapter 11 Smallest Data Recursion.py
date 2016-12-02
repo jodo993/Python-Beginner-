4de1dedfaca00df583810f417e557e5a0a575638
jodo993 <josephdo993@yahoo.com>
2016-12-01 21:38:06 -0800

    Chapter 11 Final
    find smallest element in list recursion

--- Chapter 11 Smallest Data Recursion.py
+++ Chapter 11 Smallest Data Recursion.py
@@ -1,19 +1,26 @@
+# This program finds the smallest element in the list using recursion
+
 def smallest(data):
 
+    # Return smallest
     if len(data) == 1:
         return data[0]
 
     smallestRest = smallest(data[0:-1])
-    print(data)
-    print(smallestRest)
 
+    print(smallestRest)
+    print(data)
+
+    # If the element is the smallest, return
     if smallestRest > data[-1]:
         return data[-1]
     else:
         return smallestRest
 
+# Has list of elements
 def main():
     print(smallest([10,12,33,8,52,49,23,14,1]))
     print("Expected: 1")
 
+# Start program
 main()
