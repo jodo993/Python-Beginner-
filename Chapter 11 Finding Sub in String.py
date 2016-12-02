7e434e7c9d6ebac1c266bc9513337816544eae12
jodo993 <josephdo993@yahoo.com>
2016-12-01 21:37:16 -0800

    Chapter 11 Final
    Substring find in string

--- Chapter 11 Finding Sub in String.py
+++ Chapter 11 Finding Sub in String.py
@@ -1,13 +1,19 @@
+# This program finds if a substring exist in a string using recursion
+
 def find(text, string):
+    # If first three char of text is same as substring, return true
     if text[0:3] == string:
         return True
-    else:
+    # If length of string is same as substring but no match is found, return False
+    else:               
         if len(text) == len(string):
             return False
         else:
+            # Remove first char of text
             text = text[1:]
             return find(text, string)
 
+# Send the string and substring to check if sub exists in string
 def main():
     print(find("Mississippi", "sip"))
     print("Excepted: True")
@@ -15,4 +21,5 @@ def main():
     print(find("Mississippi", "pip"))
     print("Expected: False")
 
+# Start the program
 main()
