88122c6162d419e640fb5c503adc1f4c3eeae50d
jodo993 <josephdo993@yahoo.com>
2016-12-01 21:37:49 -0800

    Chapter 11 Final
    reverse string recursion

--- Chapter 11 Reverse String Recursion.py
+++ Chapter 11 Reverse String Recursion.py
@@ -1,12 +1,18 @@
+# This program reverses a string using recursion
+
 def reverse(text):
+
+    # Take first subscript out and add it last
     if len(text) <= 1:
         return text
     else:
         return reverse(text[1:]) + text[0]
 
+# Send the string Hello to reverse function and display result
 def main():
     r = reverse("Hello!")
     print(r)
     print("Expected: !olleH")
 
+# Start program
 main()
