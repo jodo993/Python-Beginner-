515a4231c6f38fe9a92274216f117e6233670bbb
jodo993 <josephdo993@yahoo.com>
2016-12-11 19:17:59 -0800

    Extra Credit 2 Final
    Files and Exceptions: Count lines, words, chars from text

--- Extra Credit 2 - Files and Exceptions.py
+++ Extra Credit 2 - Files and Exceptions.py
@@ -1,19 +1,25 @@
+# This program counts the number of lines, words, and chars in a file.
+
 def main():
 
+    # Starting number of:
     lines = 0
     words = 0
     chars = 0
     
-    inputFile = input("What is file name: ")
+    inputFile = input("What is file name: ")    # Get file name
 
+    # Open file and use for loop to go over each line,word,char
     infile = open(inputFile, "r")
     for line in infile:
         lines = lines + 1
         words = words + len(line.split())
         chars = chars + len(line)
 
+    # Print result
     print("Total lines: ", lines)
     print("Total words: ", words)
     print("Total chars: ", chars)
-    
+
+# Start program
 main()
