63e2e474e5a8f9afa8c881c4bcdc7a822ce081f2
jodo993 <josephdo993@yahoo.com>
2016-12-11 19:19:07 -0800

    Extra Credit 2 Final
    Multi Choice answer

--- Extra Credit 2 - Inheritance.py
+++ Extra Credit 2 - Inheritance.py
@@ -1,13 +1,18 @@
+# This program allow multiple answers for a question
+# Import class
 from classQuestion import MultiChoiceQuestion
 
 def main():
-    
+
+    # Create question object and set answers and question
     question = MultiChoiceQuestion()
     question.setText("Of Apple, Tomato, Carrot, Cucumber and Celery, list all that are fruit.")
     question.setAnswer("Apple Tomato")
 
+    # Display questions and get response and check
     question.display()
     response = input("Your answer: ")
     print(question.checkAnswer(response))
-    
+
+# Start program
 main()
--- classQuestion.py
+++ classQuestion.py
@@ -1,15 +1,19 @@
 class MultiChoiceQuestion:
 
+    # Initialize question and answer
     def __init__(self):
         self._text = ""
         self._answer = ""
 
+    # Get the question
     def setText(self, questionText):
         self._text = questionText
 
+    # Get the answer
     def setAnswer(self, answerText):
         self._answer = answerText
 
+    # Compare response to answer, if match, true
     def checkAnswer(self, response):
         response = response.lower()
         response = response.split()
@@ -22,5 +26,6 @@ class MultiChoiceQuestion:
         else:
             return False
 
+    # Display question
     def display(self):
         print(self._text)
